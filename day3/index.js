f21e22eae954dc51334aaaca9da3a35022ee62ba
Ward <143821199+ahmdWard@users.noreply.github.com>
2023-11-26 19:23:14 +0200

    Add files via upload

---
+++ day3/index.js
@@ -0,0 +1,8 @@
+const inputs = document.querySelectorAll(".controls input")
+
+function handleUpdate() {
+    const sufix = this.dataset.sizing || ''
+    document.documentElement.style.setProperty(`--${this.name}`, this.value+sufix);
+}
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
